fa779984a5927e02976089dbce303bfee682d3ce
zwiegand <42557176+zwiegand@users.noreply.github.com>
2020-04-06 09:44:33 -0700

    Adding batch file to open code

---
+++ runCodeSnippetsM2.bat
@@ -0,0 +1,4 @@
+start "" https://scratch.mit.edu/projects/382732347/
+start "" https://scratch.mit.edu/projects/382779665/
+start "" https://scratch.mit.edu/projects/382726883/
+start "" https://scratch.mit.edu/projects/382730694/
